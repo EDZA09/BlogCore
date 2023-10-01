3a7df009057fca9da7b07929dbe661bbda6bb605
EDZA09 <eduarstiven2001@gmail.com>
2023-10-01 13:34:11 -0500

    Feat(Backend): Creo Modelo de Categorias

---
+++ BlogCore.Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage="Ingrese un Nombre para la Categoria")]
+        [Display(Name = "Nombre Categoria")]
+        public string Name { get; set; }
+        [Display(Name = "Orden de Visualización")]
+        public int? Order { get; set; }
+
+    }
+}
