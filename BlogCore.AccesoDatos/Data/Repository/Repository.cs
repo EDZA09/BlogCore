3e30c81cd5d8af1b65a187c9510103409d96dc86
EDZA09 <eduarstiven2001@gmail.com>
2023-10-05 20:25:34 -0500

    Feat(Backend): Implemento el IRepository y el Contexto de Datos en el Repository

--- BlogCore.AccesoDatos/Data/Repository/Repository.cs
+++ BlogCore.AccesoDatos/Data/Repository/Repository.cs
@@ -1,12 +1,54 @@
-﻿using System;
+﻿using BlogCore.DataAccess.Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BlogCore.DataAccess.Data.Repository
 {
-    internal class Repository
+    public class Repository<T> : IRepository<T> where T : class
     {
+        protected readonly DbContext Context;
+        internal DbSet<T> dbSet;
+
+        public Repository(DbContext context)
+        {
+            Context = context;
+            this.dbSet = Context.Set<T>();
+        }
+
+
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetFirstOrDefault(Expression<Func<T, bool>> filter = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
