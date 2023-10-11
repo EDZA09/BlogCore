4029610bf57b2d8c2da875a22fdc823b8e030caa
EDZA09 <eduarstiven2001@gmail.com>
2023-10-10 22:07:33 -0500

    Feat(Backend): Creo la Unidad de Trabajo, su clase y su interfaz

---
+++ BlogCore.AccesoDatos/Data/Repository/IRepository/IWorkUnity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.DataAccess.Data.Repository.IRepository
+{
+    public interface IWorkUnity : IDisposable
+    {
+        ICategoryRepository Categoria { get; }
+        //TODO:: Agregar los demas repositorios
+
+        void save();
+    }
+}
---
+++ BlogCore.AccesoDatos/Data/Repository/WorkUnity.cs
@@ -0,0 +1,24 @@
+﻿using BlogCore.DataAccess.Data.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogCore.DataAccess.Data.Repository
+{
+    public class WorkUnity : IWorkUnity
+    {
+        public ICategoryRepository Categoria => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
