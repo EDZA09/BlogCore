a2812c2a59bce2a62797cc218bde280ce0663399
EDZA09 <eduarstiven2001@gmail.com>
2023-10-11 19:41:01 -0500

    Feat(Backend): Creo controlador de Categorias en el Area de Admin

---
+++ BlogCore/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,19 @@
+﻿using BlogCore.Data;
+using BlogCore.DataAccess.Data.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogCore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoriesController : Controller
+    {
+
+        private readonly IWorkUnity _workcontainer;
+        private readonly ApplicationDbContext _ Context
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
