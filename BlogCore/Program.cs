47ad3c6ddbeb0500ec2080de68ac8eb7a14860d5
EDZA09 <eduarstiven2001@gmail.com>
2023-10-16 20:45:37 -0500

    Fix(WorkUnity): Inyecto la Unidad de Trabajo como dependencia del proyecto

--- BlogCore/Program.cs
+++ BlogCore/Program.cs
@@ -1,4 +1,6 @@
 using BlogCore.Data;
+using BlogCore.DataAccess.Data.Repository;
+using BlogCore.DataAccess.Data.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -13,6 +15,9 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+
+// Agregar contenedor de trabajo
+builder.Services.AddScoped<IWorkUnity, WorkUnity>();
 
 var app = builder.Build();
 
