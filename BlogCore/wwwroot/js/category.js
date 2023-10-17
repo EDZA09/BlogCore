ab17ec70a67507d6d53496ee6bae9750f518325b
EDZA09 <eduarstiven2001@gmail.com>
2023-10-16 22:45:06 -0500

    Fix(Backend): Arreglo referencias de atributos de categorias y errores de sintaxis

--- BlogCore/wwwroot/js/category.js
+++ BlogCore/wwwroot/js/category.js
@@ -1,29 +1,30 @@
 ﻿var dataTable;
 
-$(document).ready(func() {
+$(document).ready(function () {
     cargarDatatable();
-})
+});
+
 
 function cargarDatatable() {
-    dataTable = $("tblCategorias").DataTable({
+    dataTable = $("#tblCategorias").DataTable({
         "ajax": {
             "url": "/admin/categories/GetAll",
             "type": "GET",
-            "datatype":"json"
+            "datatype": "json"
         },
         "columns": [
             { "data": "id", "width": "5%" },
-            { "data": "nombre", "width": "50%" },
-            { "data": "orden", "width": "20%" },
+            { "data": "name", "width": "50%" },
+            { "data": "order", "width": "20%" },
             {
                 "data": "id",
                 "render": function (data) {
                     return `<div class="text-center">
-                                <a href="/Admin/Categorias/Edit/${data}" class "btn btn-success text-white" style="cursor: pointer; width:100px;">
+                                <a href="/Admin/Categories/Edit/${data}" class="btn btn-success text-white" style="cursor:pointer; width:100px;">
                                 <i class="far fa-edit"></i>Editar
                                 </a>
                                 &nbsp;
-                                <a onClick="Delete(/Admin/Categorias/Delete/${data})" class "btn btn-danger text-white" style="cursor: pointer; width:100px;">
+                                <a onclick=Delete("/Admin/Categories/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer; width:100px;">
                                 <i class="far fa-trash-alt"></i>Borrar
                                 </a>
                             </div>
@@ -41,7 +42,7 @@ function cargarDatatable() {
             "thousands": ",",
             "lengthMenu": "Mostrar _MENU_ Entradas",
             "loadingRecords": "Cargando...",
-            "processing": "Procesando",
+            "processing": "Procesando...",
             "search": "Buscar:",
             "zeroRecords": "Sin resultados encontrados",
             "paginate": {
@@ -50,6 +51,7 @@ function cargarDatatable() {
                 "next": "Siguiente",
                 "previous": "Anterior"
             }
-        }, width: "100%"
+        },
+        "width": "100%"
     });
 }
