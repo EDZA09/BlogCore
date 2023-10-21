670e32bb0830b8c63c476574cfd6eb8b96d01639
EDZA09 <eduarstiven2001@gmail.com>
2023-10-20 22:20:10 -0500

    Feat(Backend): hago una copia del ICategpryRepository para crear el IArticleRepository

---
+++ BlogCore.AccesoDatos/Data/Repository/IRepository/ICategoryRepository - Copia.cs
@@ -0,0 +1,18 @@
+﻿using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace BlogCore.DataAccess.Data.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        IEnumerable<SelectListItem>  GetListCategories();
+
+        void Update(Category category);
+
+    }
+}
