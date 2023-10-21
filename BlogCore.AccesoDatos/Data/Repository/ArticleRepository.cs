ce170f629685fdd594b4f7fc0749df5f733a5488
EDZA09 <eduarstiven2001@gmail.com>
2023-10-20 22:42:36 -0500

    Feat(Backend): Completo el ArticleRepository

---
+++ BlogCore.AccesoDatos/Data/Repository/ArticleRepository.cs
@@ -0,0 +1,34 @@
+﻿using BlogCore.Data;
+using BlogCore.DataAccess.Data.Repository.IRepository;
+using BlogCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BlogCore.DataAccess.Data.Repository
+{
+    public class ArticleRepository : Repository<Article>, IArticleRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ArticleRepository(ApplicationDbContext context) : base(context) 
+        {
+            _context = context;
+        }
+
+        public void Update(Article article)
+        {
+            var ObjectFromDB = _context.Articles.FirstOrDefault(s => s.Id == article.Id);
+            ObjectFromDB.Name = article.Name;
+            ObjectFromDB.Description = article.Description;
+            ObjectFromDB.UrlImage = article.UrlImage;
+            ObjectFromDB.CategoryId = article.CategoryId;
+
+            //_context.SaveChanges();
+        }
+    }
+}
