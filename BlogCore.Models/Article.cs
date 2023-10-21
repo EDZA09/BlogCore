80c095dd66f7e3d0c28fc1be0075c6ecf0df226a
EDZA09 <eduarstiven2001@gmail.com>
2023-10-20 21:47:50 -0500

    Feat(Backend): Agrego las propiedades al modelo de artículos

--- BlogCore.Models/Article.cs
+++ BlogCore.Models/Article.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,21 @@ namespace BlogCore.Models
 {
     public class Article
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [Display(Name = "Nombre Articulo")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria")]
+        public string Description { get; set; }
+
+        [Display(Name = "Fecha de Creación")]
+        public string CreatedAt { get; set; }
+
+        [DataType(DataType.ImageUrl)]
+        [Display(Name = "Imagen")]
+        public string UrlImage { get; set; }
     }
 }
