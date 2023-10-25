9882f68cf28f39f3ba6899ddc46b7d2b7ff1aac0
EDZA09 <eduarstiven2001@gmail.com>
2023-10-24 22:54:02 -0500

    Feat(backend): Creo una propiedad y una lista vacía en el ArticleVM

--- BlogCore.Models/ViewModels/ArticleVM.cs
+++ BlogCore.Models/ViewModels/ArticleVM.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace BlogCore.Models.ViewModels
 {
-    internal class ArticleVM
+    public class ArticleVM
     {
+        public Article article {  get; set; }
+        public IEnumerable<SelectListItem> ListaCategorias {  get; set; }
     }
 }
