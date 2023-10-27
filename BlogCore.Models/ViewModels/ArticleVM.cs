fdc8c66fa51abeaee5d1061401724e7f73480ac8
EDZA09 <eduarstiven2001@gmail.com>
2023-10-26 21:10:13 -0500

    Fix(Backend): Asigno el paquete Microsoft.AspNetCore.Mvc.Rendering a la lista de categorías

--- BlogCore.Models/ViewModels/ArticleVM.cs
+++ BlogCore.Models/ViewModels/ArticleVM.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+//using System.Web.Mvc;
 
 namespace BlogCore.Models.ViewModels
 {
